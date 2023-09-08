80101b8e58bca29eeea8a7d93b3012c59f8312a0
sarahhsallam2 <127781207+sarahhsallam2@users.noreply.github.com>
2023-09-08 21:12:56 +0300

    commit 1
    html

--- app.py
+++ app.py
@@ -20,6 +20,45 @@ def create_core_view():
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/acceptedresumes')
+def acceptedresumes():
+    return render_template('acceptedresumes.html')
+@app.route('/candidates')
+def candidates():
+    return render_template('candidates.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+@app.route('/features')
+def features():
+    return render_template('features.html')
+@app.route('/Login')
+def Login():
+    return render_template('Login.html')
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+@app.route('/product')
+def product():
+    return render_template('product.html')
+@app.route('/service')
+def service():
+    return render_template('service.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+@app.route('/startscreening')
+def startscreening():
+    return render_template('startscreening.html')
+@app.route('/register')
+def Register():
+    return render_template('Register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
